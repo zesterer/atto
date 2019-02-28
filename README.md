ecc5309793df93bfb06c1a1f6d7a13e32c8ccc43
Joshua Barretto <joshua.s.barretto@gmail.com>
2019-02-28 03:48:12 +0000

    Initial implementation of Atto

---
+++ README.md
@@ -0,0 +1,212 @@
+# Atto
+
+Atto is a ridiculously simple functional programming language for masochists.
+It features a syntax driven entirely by reverse polish notation and no delimiters to speak of.
+If you make a mistake, your code will either be misinterpreted or will simply fail for no apparent reason.
+What do you get for this simplicity? Well... an insanely simple self-evaluating language.
+
+## Syntax
+
+Basic numeric operators...
+
+```
+fn main is
+	+ 5 7
+
+# Yields 12
+```
+
+```
+fn main is
+	- * 3 3 5
+
+# Yields 4
+```
+
+Pairing values together into a two-component list
+
+```
+fn main is
+	pair 3 17
+
+# Yields [3, 17]
+```
+
+Fusing lists together
+
+```
+fn main is
+	fuse pair 3 17 pair 5 8
+
+# Yields [3, 17, 5, 8]
+```
+
+Conditional expression that return the second operand if the first evaluates to true, and the third operand if no
+
+```
+fn main is
+	if true
+		10
+		5
+
+# Yields 10
+```
+
+Selecting the first value in a list
+
+```
+fn main is
+	head pair 3 17
+
+# Yields 3
+```
+
+Selecting values trailing after the head of a list
+
+```
+fn main is
+	tail fuse 3 fuse 17 9
+
+# Yields [17, 9]
+```
+
+Converting a string into a value
+
+```
+fn main is
+	- 7 litr "3"
+
+# Yields 4
+```
+
+```
+fn main is
+	= true litr "false"
+
+# Yields false
+```
+
+```
+fn main is
+	= null litr "null"
+
+# Yields 4
+```
+
+Determining whether an item exists within a list
+
+```
+fn main is
+	in 3 fuse 1 fuse 2 fuse 3 4
+
+# Yields true
+```
+
+Defining a function with parameters
+
+```
+fn add x y is
+	+ x y
+fn main is
+	add 5 3
+
+# Yields 8
+```
+
+Recursion to find the size of a list
+
+```
+fn size l is
+	if = null tail
+		0
+		+ 1 size tail l
+fn main is
+	size fuse 1 fuse 2 3
+
+# Yields 3
+```
+
+Source code for my current implementation of the expression evaluator, in Atto:
+
+```
+fn children token is
+    if = "if" token
+		3
+    if in token fuse "head" fuse "tail" fuse "pair" "size"
+        1
+    if in token fuse "+" fuse "-" fuse "*" fuse "/" "in"
+        2
+	if ! = null litr token
+        0
+    + "Error: Unrecognised token " str token
+
+fn skip n expr is
+    if = n 0
+        expr
+	skip - n 1 tail expr
+
+fn expr_len tokens is
+    if = 0 children head tokens
+        1
+    if = 1 children head tokens
+        expr_len tail tokens
+    if = 2 children head tokens
+        + expr_len tail tokens
+          skip expr_len tail tokens tail tokens
+    + "Unrecognised expression head: " str head tokens
+
+fn nth_expr n expr is
+    if = n 0
+        expr
+    nth_expr - n 1 skip expr_len expr expr
+
+fn eval tokens is
+    if = "if" head tokens
+        if eval nth_expr 0 tail tokens
+            eval nth_expr 1 tail tokens
+            eval nth_expr 2 tail tokens
+    if = "head" head tokens
+        head eval nth_expr 0 tail tokens
+    if = "tail" head tokens
+        tail eval nth_expr 0 tail tokens
+    if = "fuse" head tokens
+        fuse eval nth_expr 0 tail tokens
+			 eval nth_expr 1 tail tokens
+    if = "pair" head tokens
+        pair eval nth_expr 0 tail tokens
+			 eval nth_expr 1 tail tokens
+    if = "size" head tokens
+        size eval nth_expr 0 tail tokens
+    if = "litr" head tokens
+        litr eval nth_expr 0 tail tokens
+    if = "str" head tokens
+        str eval nth_expr 0 tail tokens
+    if = "words" head tokens
+        words eval nth_expr 0 tail tokens
+	if = "!" head tokens
+        ! eval nth_expr 0 tail tokens
+    if = "+" head tokens
+        + eval nth_expr 0 tail tokens
+          eval nth_expr 1 tail tokens
+    if = "-" head tokens
+	    - eval nth_expr 0 tail tokens
+          eval nth_expr 1 tail tokens
+    if = "*" head tokens
+        * eval nth_expr 0 tail tokens
+          eval nth_expr 1 tail tokens
+    if = "/" head tokens
+        / eval nth_expr 0 tail tokens
+          eval nth_expr 1 tail tokens
+	if = "=" head tokens
+        = eval nth_expr 0 tail tokens
+          eval nth_expr 1 tail tokens
+    if = "in" head tokens
+        in eval nth_expr 0 tail tokens
+           eval nth_expr 1 tail tokens
+    if ! = null litr head tokens
+        litr head tokens
+    + "Error: Unrecognised token " str head tokens
+
+fn main code is
+    eval words code
+```
---
+++ src/main.rs
@@ -0,0 +1,345 @@
+use std::{
+    slice,
+    collections::HashMap,
+};
+
+#[derive(Debug)]
+enum Error {
+    Expected(Token),
+    ExpectedToken,
+    Unexpected(Token),
+    CannotFind(String),
+}
+
+#[derive(Clone, Debug, PartialEq)]
+enum Value {
+    Num(f64),
+    Str(String),
+    Bool(bool),
+    List(Vec<Value>),
+    Null,
+}
+
+impl Value {
+    pub fn from_str(s: &str) -> Option<Value> {
+        if s == "null" {
+            Some(Value::Null)
+        } else if s == "true" {
+            Some(Value::Bool(true))
+        } else if s == "false" {
+            Some(Value::Bool(false))
+        } else if let Ok(x) = s.parse() {
+            Some(Value::Num(x))
+        } else if let Ok(b) = s.parse() {
+            Some(Value::Bool(b))
+        } else if s.chars().next() == Some('"' /*"*/) {
+            Some(Value::Str(s[1..].to_string()))
+        } else {
+            None
+        }
+    }
+
+    pub fn into_string(self) -> String {
+        match self {
+            Value::Num(x) => format!("{}", x),
+            Value::Str(s) => s,
+            Value::Bool(b) => format!("{}", b),
+            Value::List(_) => "<list>".to_string(),
+            Value::Null => "null".to_string(),
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+enum Token {
+    Fn, Is,
+
+    In,   If,   Head, Tail,
+    Fuse, Pair, Litr, Str,
+    Words,
+
+    Not, Eq, Add, Sub, Mul, Div,
+
+    Value(Value),
+    Ident(String),
+}
+
+#[derive(Debug)]
+enum Expr {
+    If(Box<Expr>, Box<Expr>, Box<Expr>),
+    In(Box<Expr>, Box<Expr>),
+    Head(Box<Expr>),
+    Tail(Box<Expr>),
+    Fuse(Box<Expr>, Box<Expr>),
+    Pair(Box<Expr>, Box<Expr>),
+    Litr(Box<Expr>),
+    Str(Box<Expr>),
+    Words(Box<Expr>),
+
+    Not(Box<Expr>),
+    Eq(Box<Expr>, Box<Expr>),
+    Add(Box<Expr>, Box<Expr>),
+    Sub(Box<Expr>, Box<Expr>),
+    Mul(Box<Expr>, Box<Expr>),
+    Div(Box<Expr>, Box<Expr>),
+
+    Value(Value),
+    Call(String, Vec<Expr>),
+    Local(usize),
+}
+
+#[derive(Debug)]
+struct Func {
+    args: Vec<String>,
+    expr: Expr,
+}
+
+fn eval(expr: &Expr, funcs: &HashMap<String, Func>, args: &Vec<Value>) -> Value {
+    match expr {
+        Expr::If(pred, good, bad) => if eval(&pred, funcs, args) == Value::Bool(true) {
+            eval(&good, funcs, args)
+        } else {
+            eval(&bad, funcs, args)
+        },
+        Expr::In(expr, list) => match eval(&list, funcs, args) {
+            Value::List(items) => Value::Bool(items.contains(&eval(&expr, funcs, args))),
+            _ => Value::Null
+        },
+        Expr::Not(x) => match eval(&x, funcs, args) {
+            Value::Bool(b) => Value::Bool(!b),
+            _ => Value::Null,
+        },
+        Expr::Eq(x, y) => Value::Bool(eval(&x, funcs, args) == eval(&y, funcs, args)),
+        Expr::Add(x, y) => match (eval(&x, funcs, args), eval(&y, funcs, args)) {
+            (Value::Num(x), Value::Num(y)) => Value::Num(x + y),
+            (Value::Str(x), Value::Str(y)) => Value::Str(x + &y),
+            _ => Value::Null,
+        },
+        Expr::Sub(x, y) => match (eval(&x, funcs, args), eval(&y, funcs, args)) {
+            (Value::Num(x), Value::Num(y)) => Value::Num(x - y),
+            _ => Value::Null,
+        },
+        Expr::Mul(x, y) => match (eval(&x, funcs, args), eval(&y, funcs, args)) {
+            (Value::Num(x), Value::Num(y)) => Value::Num(x * y),
+            _ => Value::Null,
+        },
+        Expr::Div(x, y) => match (eval(&x, funcs, args), eval(&y, funcs, args)) {
+            (Value::Num(x), Value::Num(y)) => Value::Num(x / y),
+            _ => Value::Null,
+        },
+        Expr::Head(list) => if let Value::List(items) = eval(&list, funcs, args) {
+            items.first().cloned().unwrap_or(Value::Null)
+        } else {
+            Value::Null
+        },
+        Expr::Tail(list) => if let Value::List(items) = eval(&list, funcs, args) {
+            items.get(1..).map(|items| Value::List(items.iter().cloned().collect())).unwrap_or(Value::Null)
+        } else {
+            Value::Null
+        },
+        Expr::Fuse(x, y) => match (eval(&x, funcs, args), eval(&y, funcs, args)) {
+            (Value::List(mut x), Value::List(mut y)) => Value::List({ x.append(&mut y); x }),
+            (Value::List(mut x), y) => Value::List({ x.push(y); x }),
+            (x, Value::List(mut y)) => Value::List({ let mut v = vec![x]; v.append(&mut y); v }),
+            (x, y) => Value::List(vec![x, y]),
+        },
+        Expr::Pair(x, y) => Value::List(vec![eval(&x, funcs, args), eval(&y, funcs, args)]),
+        Expr::Call(f, params) => if let Some(f) = funcs.get(f) {
+            eval(&f.expr, funcs, &params.iter().map(|p| eval(&p, funcs, args)).collect())
+        } else {
+            Value::Null
+        },
+        Expr::Words(x) => if let Value::Str(s) = eval(&x, funcs, args) {
+            Value::List(words(&s).into_iter().map(|s| Value::Str(s)).collect())
+        } else {
+            Value::Null
+        },
+        Expr::Litr(x) => if let Value::Str(s) = eval(&x, funcs, args) {
+            Value::from_str(&s).unwrap_or(Value::Null)
+        } else {
+            Value::Null
+        },
+        Expr::Str(x) => Value::Str(eval(&x, funcs, args).into_string()),
+        Expr::Value(val) => val.clone(),
+        Expr::Local(idx) => args[*idx].clone(),
+        expr => panic!("Expr: {:?}", expr),
+    }
+}
+
+fn parse_expr(tokens: &mut slice::Iter<Token>, args: &Vec<String>, func_defs: &HashMap<String, usize>) -> Result<Expr, Error> {
+    Ok(match tokens.next().ok_or(Error::ExpectedToken)? {
+        Token::If => Expr::If(
+            Box::new(parse_expr(tokens, args, func_defs)?),
+            Box::new(parse_expr(tokens, args, func_defs)?),
+            Box::new(parse_expr(tokens, args, func_defs)?),
+        ),
+        Token::In => Expr::In(
+            Box::new(parse_expr(tokens, args, func_defs)?),
+            Box::new(parse_expr(tokens, args, func_defs)?),
+        ),
+        Token::Head => Expr::Head(Box::new(parse_expr(tokens, args, func_defs)?)),
+        Token::Tail => Expr::Tail(Box::new(parse_expr(tokens, args, func_defs)?)),
+        Token::Fuse => Expr::Fuse(
+            Box::new(parse_expr(tokens, args, func_defs)?),
+            Box::new(parse_expr(tokens, args, func_defs)?),
+        ),
+        Token::Pair => Expr::Pair(
+            Box::new(parse_expr(tokens, args, func_defs)?),
+            Box::new(parse_expr(tokens, args, func_defs)?),
+        ),
+        Token::Litr => Expr::Litr(Box::new(parse_expr(tokens, args, func_defs)?)),
+        Token::Str => Expr::Str(Box::new(parse_expr(tokens, args, func_defs)?)),
+        Token::Words => Expr::Words(Box::new(parse_expr(tokens, args, func_defs)?)),
+        Token::Value(v) => Expr::Value(v.clone()),
+
+        Token::Not => Expr::Not(Box::new(parse_expr(tokens, args, func_defs)?)),
+        Token::Eq => Expr::Eq(
+            Box::new(parse_expr(tokens, args, func_defs)?),
+            Box::new(parse_expr(tokens, args, func_defs)?),
+        ),
+        Token::Add => Expr::Add(
+            Box::new(parse_expr(tokens, args, func_defs)?),
+            Box::new(parse_expr(tokens, args, func_defs)?),
+        ),
+        Token::Sub => Expr::Sub(
+            Box::new(parse_expr(tokens, args, func_defs)?),
+            Box::new(parse_expr(tokens, args, func_defs)?),
+        ),
+        Token::Mul => Expr::Mul(
+            Box::new(parse_expr(tokens, args, func_defs)?),
+            Box::new(parse_expr(tokens, args, func_defs)?),
+        ),
+        Token::Div => Expr::Div(
+            Box::new(parse_expr(tokens, args, func_defs)?),
+            Box::new(parse_expr(tokens, args, func_defs)?),
+        ),
+
+        Token::Ident(i) => {
+            if let Some((idx, _)) = args
+                .iter()
+                .enumerate()
+                .find(|(_, arg)| &i == arg)
+            {
+                Expr::Local(idx)
+            } else if let Some(f_args) = func_defs.get(i.as_str()) {
+                let mut params = vec![];
+                for _ in 0..*f_args {
+                    params.push(parse_expr(tokens, args, func_defs)?);
+                }
+                Expr::Call(i.clone(), params)
+            } else {
+                return Err(Error::CannotFind(i.clone()));
+            }
+        },
+        t => return Err(Error::Unexpected(t.clone())),
+    })
+}
+
+fn parse_funcs(mut tokens: slice::Iter<Token>) -> Result<HashMap<String, Func>, Error> {
+    let mut funcs = HashMap::new();
+    let mut func_defs = HashMap::new();
+    loop {
+        match tokens.next() {
+            Some(Token::Fn) => {},
+            t => {
+                println!("Found {:?}", t);
+                return Ok(funcs)
+            },
+        }
+
+        let name = match tokens.next() {
+            Some(Token::Ident(s)) => s.clone(),
+            _ => return Err(Error::Expected(Token::Fn)),
+        };
+
+        let mut args = vec![];
+        loop {
+            match tokens.next() {
+                Some(Token::Ident(s)) => args.push(s.clone()),
+                Some(Token::Is) => break,
+                _ => return Err(Error::Expected(Token::Is)),
+            }
+        }
+
+        func_defs.insert(name.clone(), args.len());
+
+        let expr = parse_expr(&mut tokens, &args, &func_defs)?;
+
+        funcs.insert(name, Func {
+            args,
+            expr,
+        });
+    }
+}
+
+fn words(s: &str) -> Vec<String> {
+    s
+        .chars()
+        .scan((false, String::new()), |(in_str, buf), c| {
+            match c {
+                '"' /*"*/ => if *in_str {
+                    *in_str = false;
+                } else {
+                    buf.push('"' /*"*/);
+                    *in_str = true;
+                },
+                c if c.is_whitespace() && !*in_str => {
+                    let s = buf.clone();
+                    buf.clear();
+                    return Some(s);
+                },
+                c => buf.push(c),
+            }
+            Some("".to_string())
+        })
+        .filter(|s| s.len() > 0)
+        .collect()
+}
+
+fn main() {
+    let code = include_str!("eval.at");
+
+    let tokens = words(code)
+        .into_iter()
+        .map(|s| match s.as_str() {
+            "fn" => Token::Fn,
+            "is" => Token::Is,
+            "in" => Token::In,
+            "if" => Token::If,
+            "head" => Token::Head,
+            "tail" => Token::Tail,
+            "fuse" => Token::Fuse,
+            "pair" => Token::Pair,
+            "litr" => Token::Litr,
+            "str" => Token::Str,
+            "words" => Token::Words,
+            "=" => Token::Eq,
+            "+" => Token::Add,
+            "-" => Token::Sub,
+            "*" => Token::Mul,
+            "/" => Token::Div,
+            "!" => Token::Not,
+            s => if let Some(v) = Value::from_str(s) {
+                Token::Value(v)
+            } else {
+                Token::Ident(s.to_string())
+            }
+        })
+        .collect::<Vec<_>>();
+
+    let funcs = parse_funcs(tokens.iter()).unwrap();
+
+    let example = r#"
+    + 5 * 2 3
+    "#;
+
+    let result = eval(
+        &funcs.get("main").unwrap().expr,
+        &funcs,
+        &mut vec![
+            Value::Str(example.to_string())
+        ],
+    );
+
+    println!("Result: {:?}", result);
+}
